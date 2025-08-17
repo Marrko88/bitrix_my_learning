9beb039df0e917a36061702bb2e7a1a9517479cf
Marrko88 <markianec@mail.ru>
2025-08-17 11:52:56 +0300

    add testgrid/index.php

--- testgrid/index.php
+++ testgrid/index.php
@@ -1,4 +1,5 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->IncludeComponent("company:testgrid", "", []);
+echo "ttt";
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
